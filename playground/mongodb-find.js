e49ec92ba526b4d4905a720cfd9a506fd6508d3e
thomas <nealthom@gmail.com>
2018-04-06 17:40:15 -0500

    add delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,14 +7,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
   }
   console.log('Connected to MongoDB server');
 
-// db.collection('Todos').find({
-//   _id: new ObjectID('5ac686b1fc58100efb24092e')
-// }).toArray().then((docs)=>{
-//   console.log('Todos');
-//   console.log(JSON.stringify(docs,undefined,2));
-// },(err)=>{
-//   console.log('Unable to fetch todos',err);
-// });
 db.collection('Todos').find().count().then((count)=>{
   console.log(`Todos count: ${count}`);
 },(err)=>{
